0d25791e7c21fb9b28ea8b09bbe4c2a6479b91f1
luixaviles <luis.avilesz@gmail.com>
2018-03-20 14:17:20 -0400

    Clase #6

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
-    entry: "./src/app.ts",
+    entry: "./src/clase-06/app.ts",
     output: {
       filename: 'app.js',
       path: __dirname + './dist'
